a18b04b8fd62bf1f0e49788f64cce7d20f7b204a
codeoflife <codeoflife@gmail.com>
2019-03-17 01:32:58 -0400

    Add files via upload

--- common/TextDialog.java
+++ common/TextDialog.java
@@ -1,60 +1,126 @@
 package common;
 
+import java.awt.AWTException;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
 
-
-
-public class TextDialog extends JFrame {
+public class TextDialog extends JDialog {
 	private static final long serialVersionUID = 1L;
-	
+
 	public WaitingThread waitingThread;
-	
+
 	private JLabel label;
 	public JLabel showTimeLabel;
 	private JButton button;
-	
+
 	/* Time text fields */
 	public JTextField hoursTextField;
 	public JTextField minutesTextField;
 	public JTextField secondsTextField;
+
+	SystemTray tray;
+	TrayIcon trayIcon;
+
+	private void hideToSystemTray(final JDialog frame) {
+		if (!SystemTray.isSupported())
+			return;
+		System.out.println("System tray supported");
+		tray = SystemTray.getSystemTray();
+
+		ActionListener exitListener = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Exiting....");
+				System.exit(0);
+			}
+		};
+		PopupMenu popup = new PopupMenu();
+		MenuItem defaultItem = new MenuItem("Exit");
+		defaultItem.addActionListener(exitListener);
+		popup.add(defaultItem);
 	
-	
-	public TextDialog(Monitor monitor, WaitingThread waitingThread) {
-		super("WiggleMouse");
-		this.waitingThread = waitingThread;
-		
-		/* This will make the program look like any other program and not
-		 * have the ugly default swing look
-		 * */
-		try {  
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());  
-		}  catch(Exception e) {  
-			System.out.println("Couldn't set native look and feel");
+		defaultItem = new MenuItem("Open");
+		defaultItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.setVisible(true);
+				//frame.setExtendedState(JFrame.NORMAL);
+			}
+		});
+		popup.add(defaultItem);
+		//Image image = Toolkit.getDefaultToolkit().getImage("mouse.png");
+		Image image=null;
+		try {
+			image = ImageIO.read(TextDialog.class.getResource("mouse.png"));
+		} catch (IOException e1) {
+			e1.printStackTrace();
 		}
 		
+		trayIcon = new TrayIcon(image, waitingThread.getStatus(), popup);
+		trayIcon.setImageAutoSize(true);
+		setIconImage(image);
+		ActionListener listener = new ActionListener()
+	    {
+	        public void actionPerformed(ActionEvent e)
+	        {
+				frame.setVisible(true);
+				//frame.setExtendedState(JFrame.NORMAL);
+	        }
+	    };	
+	    trayIcon.addActionListener(listener);		
+		//setVisible(true);
+		//setSize(300, 200);
+		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		
+        try {
+            tray.add(trayIcon);
+        } catch (AWTException e) {
+            System.out.println("TrayIcon could not be added.");
+        }		
+	}
+
+	public TextDialog(Monitor monitor, WaitingThread waitingThread) {
+		//super("WiggleMouse");
+		this.setTitle("WiggleMouse");
+		this.waitingThread = waitingThread;
+
+		/*
+		 * This will make the program look like any other program and not have the ugly default swing look
+		 */
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			System.out.println("Couldn't set native look and feel");
+		}
+
 		/* Creating widgets */
 		this.label = new JLabel("Time to wait before mouse wiggle:");
-		String showTime = monitor.getHours()+"h:"+monitor.getMinutes()+"m:"+monitor.getSeconds()+"s";
+		String showTime = monitor.getHours() + "h:" + monitor.getMinutes() + "m:" + monitor.getSeconds() + "s";
 		this.showTimeLabel = new JLabel(showTime);
-		this.hoursTextField = new JTextField(monitor.getHours()+"", 2);
-		this.minutesTextField = new JTextField(monitor.getMinutes()+"", 2);
-		this.secondsTextField = new JTextField(monitor.getSeconds()+"", 2);
-		
+		this.hoursTextField = new JTextField(monitor.getHours() + "", 2);
+		this.minutesTextField = new JTextField(monitor.getMinutes() + "", 2);
+		this.secondsTextField = new JTextField(monitor.getSeconds() + "", 2);
+
 		this.button = new JButton("Set time");
-		
-		
+
 		/* Layout stuff */
 		this.setLayout(new GridLayout(3, 1));
 		JPanel mainLabelPanel = new JPanel();
@@ -63,21 +129,21 @@ public class TextDialog extends JFrame {
 		mainLabelPanel.setLayout(new FlowLayout());
 		showTimeLabelPanel.setLayout(new FlowLayout());
 		textFieldPanel.setLayout(new FlowLayout());
-		
+
 		this.add(mainLabelPanel);
 		this.add(showTimeLabelPanel);
 		this.add(textFieldPanel);
-		
+
 		mainLabelPanel.add(label);
 		showTimeLabelPanel.add(showTimeLabel);
 		textFieldPanel.add(hoursTextField);
 		textFieldPanel.add(minutesTextField);
 		textFieldPanel.add(secondsTextField);
 		textFieldPanel.add(button);
-		
+
 		/* Misc layout */
 		Font curFont = showTimeLabel.getFont();
-	    showTimeLabel.setFont(new Font(curFont.getFontName(), curFont.getStyle(), 20));
+		showTimeLabel.setFont(new Font(curFont.getFontName(), curFont.getStyle(), 20));
 		this.setSize(250, 200);
 
 		/* Listeners */
@@ -87,12 +153,15 @@ public class TextDialog extends JFrame {
 		this.minutesTextField.addActionListener(timeListener);
 		this.secondsTextField.addActionListener(timeListener);
 		this.addWindowListener(new FrameListener());
-		
-		
+
 		/* Show it */
-		this.setVisible(true);
-		minutesTextField.requestFocus();
+		// this.setVisible(true);
+		// minutesTextField.requestFocus();
+		hideToSystemTray(this);
+		this.trayIcon.setToolTip(waitingThread.status);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		
+
 	}
 }
 
@@ -101,24 +170,25 @@ public class TextDialog extends JFrame {
 class SetTimeListener implements ActionListener {
 	private TextDialog textDialog;
 	private Monitor monitor;
+
 	public SetTimeListener(TextDialog textDialog, Monitor monitor) {
 		this.textDialog = textDialog;
 		this.monitor = monitor;
 	}
-	
+
 	public void actionPerformed(ActionEvent event) {
 		int hours = 0;
 		int minutes = 0;
 		int seconds = 0;
-		
+
 		try {
 			seconds = Integer.parseInt(textDialog.secondsTextField.getText());
 			minutes = seconds / 60;
-			seconds = seconds%60;
+			seconds = seconds % 60;
 		} catch (NumberFormatException e) {
 			seconds = 1;
 		}
-		
+
 		try {
 			minutes += Integer.parseInt(textDialog.minutesTextField.getText());
 			hours = minutes / 60;
@@ -126,7 +196,7 @@ class SetTimeListener implements ActionListener {
 		} catch (Exception e) {
 			minutes = 0;
 		}
-		
+
 		try {
 			hours += Integer.parseInt(textDialog.hoursTextField.getText());
 			if (hours > 100) {
@@ -135,29 +205,29 @@ class SetTimeListener implements ActionListener {
 		} catch (NumberFormatException e) {
 			hours = 0;
 		}
-		
+
 		/* Otherwise it will do some crazy wiggling */
 		if (hours == 0 && minutes == 0 && seconds == 0) {
 			seconds = 1;
 		}
-		
-		textDialog.hoursTextField.setText(hours+"");
-		textDialog.minutesTextField.setText(minutes+"");
-		textDialog.secondsTextField.setText(seconds+"");
-		
-		String showTime = hours+"h:"+minutes+"m:"+seconds+"s";
+
+		textDialog.hoursTextField.setText(hours + "");
+		textDialog.minutesTextField.setText(minutes + "");
+		textDialog.secondsTextField.setText(seconds + "");
+
+		String showTime = hours + "h:" + minutes + "m:" + seconds + "s";
 		textDialog.showTimeLabel.setText(showTime);
-		
+
 		monitor.setTime(hours, minutes, seconds);
+		textDialog.waitingThread.setMonitor(monitor);
 		textDialog.waitingThread.interrupt();
+		textDialog.trayIcon.setToolTip(textDialog.waitingThread.status);
 	}
+
 }
-
-
 
 class FrameListener extends WindowAdapter {
-	public void windowClosing(WindowEvent e){
-		System.exit(0);
+	public void windowClosing(WindowEvent e) {
+		//System.exit(0);
 	}
 }
-
--- main/WiggleMouse.java
+++ main/WiggleMouse.java
@@ -1,5 +1,8 @@
 package main;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import common.Monitor;
 import common.TextDialog;
 import common.WaitingThread;
@@ -7,66 +10,58 @@ import common.WaitingThread;
 public class WiggleMouse {
 
 	public static void main(String[] args) {
-
+		System.out.println("Usage: java -jar wigglemouse [-interval hh:mm:ss]");
 		Monitor monitor = new Monitor();
 
-		boolean gui = true;
-		int hours = 0;
-		int minutes = 5;
-		int seconds = 0;
-
-		try {
-			
-			/* Parse arguments. The two possible arguments are:
-			 * -nogui
-			 * -interval hh:mm:ss
-			 * */
-			for (int i = 0; i < args.length; i++) {
-				if (args[i].equals("-interval")) {
-					i++;
-					String times[] = args[i].split(":");
-					hours = Integer.parseInt(times[0]);
-					minutes = Integer.parseInt(times[1]);
-					seconds = Integer.parseInt(times[2]);
-
-					if (minutes > 60 || seconds > 60) {
-						// TODO: Create specific exception
-						throw new Exception();
+		if(args.length>0)
+		{
+			try {
+				int hours = 0;
+				int minutes = 10;
+				int seconds = 0;
+	
+				/* Parse arguments. The two possible arguments are:
+				 * -nogui
+				 * -interval hh:mm:ss
+				 * */
+				for (int i = 0; i < args.length; i++) {
+					if (args[i].equals("-interval")) {
+						i++;
+						String times[] = args[i].split(":");
+						hours = Integer.parseInt(times[0]);
+						minutes = Integer.parseInt(times[1]);
+						seconds = Integer.parseInt(times[2]);
+	
+						if (minutes > 60 || seconds > 60) {
+							throw new Exception();
+						}
 					}
-					
-					if (hours == 0 && minutes == 0 && seconds == 0) {
-						// TODO: Create specific exception
-						throw new Exception("The delay must be greater than zero seconds");
-					}
-				} else if (args[i].equals("-nogui")) {
-					gui = false;
-				} else {
-					// TODO: Create specific exception
-					throw new Exception("No such argument");
 				}
-			}
-			
-			monitor.setTime(hours, minutes, seconds);
-			
-		} catch (NullPointerException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			System.err.println("Usage: java -jar wigglemouse [-nogui] [-interval hh:mm:ss]");
-			System.exit(1);
-		} 
+				
+				monitor.setTime(hours, minutes, seconds);
+				
+			} catch (NullPointerException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				System.err.println("Usage: java -jar wigglemouse [-interval hh:mm:ss]");
+				System.exit(1);
+			} 
+		}
+
 
 		/* We'll create a thread that will sleep for some seconds and
 		 * after those seconds it will wiggle the mouse 
 		 * */
 		WaitingThread waitingThread = new WaitingThread(monitor);
-		
-		if (gui) {
-			TextDialog textDialog = new TextDialog(monitor, waitingThread);	
-		} else {
-			System.out.println("Running with interval: "+hours+":"+minutes+":"+seconds);
-		}
-		
-
 		waitingThread.start();
+		
+		final TextDialog textDialog = new TextDialog(monitor, waitingThread);
+		textDialog.addWindowListener(new WindowAdapter() {
+		    public void windowClosing(WindowEvent e) {
+		    	textDialog.setVisible(false);
+		    	//textDialog.setExtendedState(JFrame.ICONIFIED);
+		    }
+		});
+
 	}
 }
